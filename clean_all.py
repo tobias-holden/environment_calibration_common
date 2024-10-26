6ec952bc70c75f78719a5f3b8615c84cf5d2f26c
tobias-holden <tobias.holden@northwestern.ed>
2024-10-25 23:28:35 -0500

    add clean_COMPS_ID

--- clean_all.py
+++ clean_all.py
@@ -15,9 +15,17 @@ def clean_analyzers():
 def clean_logs():
     log_dir= manifest.CURRENT_DIR / "log"
     for file in os.listdir(log_dir):
-            os.remove(os.path.join(log_dir,file))
+        os.remove(os.path.join(log_dir,file))
+
+def clean_COMPS_ID():
+    COMPS_ID_dir = manifest.CURRENT_DIR / "COMPS_ID"
+    for file in os.listdir(COMPS_ID_dir):
+        if 'version.txt' in file:
+            continue
+        os.remove(os.path.join(COMPS_ID_dir,file))      
 
 
 if __name__ == "__main__":
     clean_analyzers()
     clean_logs()
+    clean_COMPS_ID()
